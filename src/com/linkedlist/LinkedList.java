e38f9d14c28612251726f0b55d426bc9210ffe20
stutisaurya <stuti.saurya@gmail.com>
2021-07-02 21:44:09 +0530

    Added:AppendLinkedListForward

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -3,16 +3,18 @@ package com.linkedlist;
 public class LinkedList {
 	public static void main(String[] args) {
 		LinkedListOperation operation = new LinkedListOperation();
-		INode head;
-		Node<Integer> myFirstNode = new Node<>(70);
-		Node<Integer> mySecondNode = new Node<>(30);
-		Node<Integer> myThirdNode = new Node<>(56);
 
-		head = operation.addNode(myFirstNode);
-		operation.displayNode(head);
-		head = operation.addNode(mySecondNode);
-		operation.displayNode(head);
-		head = operation.addNode(myThirdNode);
-		operation.displayNode(head);
+		Node<Integer> myFirstNode = new Node<>(56);
+		Node<Integer> mySecondNode = new Node<>(30);
+		Node<Integer> myThirdNode = new Node<>(70);
+
+		operation.addNode(myFirstNode);
+
+		operation.addNode(mySecondNode);
+
+		operation.addNode(myThirdNode);
+
+		operation.displayNode();
+
 	}
 }
--- src/com/linkedlist/LinkedListOperation.java
+++ src/com/linkedlist/LinkedListOperation.java
@@ -1,21 +1,35 @@
 package com.linkedlist;
 
 public class LinkedListOperation {
-	private INode head;
+	// Represent the head and tail of the singly linked list
+	public Node head = null;
+	public Node tail = null;
 
-	public INode addNode(INode myNode) {
-		if (head == null)
-			head = myNode;
-		else {
-			INode temp = head;
-			head = myNode;
-			head.setNext(temp);
+	public Node addNode(Node newNode) {
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
 		}
 		return head;
 	}
 
-	public void displayNode(INode head) {
-		System.out.println(head.getKey());
+	public void displayNode() {
+		INode current = head;
+		if (head == null)
+			System.out.println("Linked List is empty");
+		System.out.print("Nodes are: ");
+		while (current != null) {
+			if (current.getNext() != null) {
+				System.out.print(current.getKey() + " -> ");
+				current = current.getNext();
+			} else {
+				System.out.println(current.getKey());
+				current = current.getNext();
+			}
+		}
 	}
 
 }
